1e1ab992392ed39ab92126ae950f66851971e77e
Theodoros Stouraitis <t.stouraitis@deepsea.ai>
2025-02-28 21:04:55 +0200

    Added extra simple example with random actions

---
+++ example_random_actions.py
@@ -0,0 +1,33 @@
+import gymnasium as gym
+from gymnasium.wrappers.record_video import RecordVideo
+import sailboat_gym
+import cv2
+
+env = gym.make('ShipEnv-v0', renderer=sailboat_gym.CV2DRenderer())
+# env = RecordVideo(env, video_folder='./output/videos/')
+
+env.reset(seed=10)
+
+for i in range(100):
+    act = env.action_space.sample()
+    obs, reward, terminated, truncated, info = env.step(act)
+    if truncated:
+        break
+    # env.render()
+
+    # Get the RGB frame
+    frame = env.render()
+
+    # Convert RGB to BGR for OpenCV
+    frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+
+    # Show the frame in OpenCV window
+    cv2.imshow("Gymnasium Environment", frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):  # Press 'q' to quit
+        break
+    input()
+
+env.close()
+
+ 
