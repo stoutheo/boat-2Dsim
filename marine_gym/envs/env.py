9c3ff5817af56a6d3e4e5370825bb9019273e1db
Theodoros Stouraitis <t.stouraitis@deepsea.ai>
2025-03-04 22:57:29 +0200

    Update on interface

--- marine_gym/envs/env.py
+++ marine_gym/envs/env.py
@@ -7,8 +7,6 @@ from ..utils import ProfilingMeta, is_debugging_all
 from ..abstracts import AbcRender
 
 
-import marine_gym.simulators.fortran_based.fortran_sim as fortr_sim
-
 
 class Metadata(TypedDict):
     render_modes: list[str]
@@ -74,9 +72,8 @@ def dummy_update(obs_dict):
 
 
 
-def fortran_sim_update(obs_dict, actions, simulator):
+def sim_update(obs_dict, actions, simulator):
     
-    # unwrap to match fortran simulator format
     init_state = np.array([ obs_dict['p_boat'][0],    obs_dict['p_boat'][1],    obs_dict['theta_boat'][0],
                             obs_dict['dt_p_boat'][0], obs_dict['dt_p_boat'][1], obs_dict['dt_theta_boat'][0],  
                             obs_dict['rpm_me'][0], np.rad2deg(obs_dict['theta_rudder'][0]), 
@@ -153,43 +150,143 @@ class ShipEnv(VesselEnv):
             'render_fps': float(video_speed * self.NB_STEPS_PER_SECONDS),
         }
 
-        def direction_generator(std=1.):
-            direction = np.random.normal(0, std, 2)
-
-            def generate_direction(step_idx):
-                return direction
-            return generate_direction
-
         self.name = name
         self.reward_fn = reward_fn
         self.stop_condition_fn = stop_condition_fn
         self.renderer = renderer
         self.obs = None
-        self.wind_generator_fn = wind_generator_fn if wind_generator_fn \
-            else direction_generator()
-        self.water_generator_fn = water_generator_fn if water_generator_fn \
-            else direction_generator(0.01)
-        self.wave_generator_fn = wave_generator_fn if wave_generator_fn \
-            else direction_generator(0.01)
+        
         self.map_scale = map_scale
         self.keep_sim_alive = keep_sim_alive
-        self.step_idx = 0
 
-    def reset(self, seed=None, **kwargs):
+        # initialize simulator
+        self.sim = A()
+
+
+    def _direction_generator(self, std=3.):
+
+            angle = np.random.uniform(-180, 180, 1) 
+            speed = np.random.normal(0, std, 1)
+
+            def generate_direction(step_idx):
+                return np.array([angle, speed])
+            
+            return generate_direction
+    
+    def _map_vessel_state_2_observations(self, obs_dict, state):
+        '''
+        map vessel state to obs_dict
+        
+        vessel state: [x,   y,  theta,     u,     v,       r,     n,  delta, fb,   fs]
+                      (m), (m), (rad), (m/s), (m/s), (rad/s), (rpm),  (deg), (N),  (N)
+
+
+        obs_dict: [p_boat[2],  theta_boat[1],  dt_p_boat[2],  dt_theta_boat[1],  rpm_me[1],  delta[1],  fb[1],   fs[1]]
+                    (m), (m),          (rad),  (m/s), (m/s),           (rad/s),      (rpm),     (rad),    (N),     (N)
+        
+        '''
+
+        obs_dict['p_boat'][0] = state[0,0]
+        obs_dict['p_boat'][1] = state[1,0]
+        obs_dict['theta_boat'][0] = state[2,0]
+
+        obs_dict['dt_p_boat'][0] = state[3,0]
+        obs_dict['dt_p_boat'][1] = state[4,0]  
+        obs_dict['dt_theta_boat'][0] = state[5,0]
+
+        obs_dict['rpm_me'][0] = state[6,0] 
+
+        obs_dict['theta_rudder'][0] = np.deg2rad(state[7,0])
+        
+        obs_dict['N_thrusters'][0] = state[8,0]
+        obs_dict['N_thrusters'][1] = state[9,0]
+        
+        return obs_dict
+    
+    def _map_env_state_2_observations(self, obs_dict, current, wind, wave):
+        '''
+        map state to obs_dict
+        
+        env state: {     current,          wind,      wave}
+                    (deg), (m/s),  (deg), (m/s), (deg), (m)
+
+
+        obs_dict: [p_boat[2],  theta_boat[1],  dt_p_boat[2],  dt_theta_boat[1],  rpm_me[1],  delta[1],  fb[1],   fs[1]]
+                    (m), (m),          (rad),  (m/s), (m/s),           (rad/s),      (rpm),     (rad),    (N),     (N)
+        
+        '''
+
+        
+        obs_dict['current'][0] = np.deg2rad(current[0])
+        obs_dict['current'][1] = current[1]
+
+        obs_dict['wind'][0] = np.deg2rad(wind[0])
+        obs_dict['wind'][1] = wind[1]
+
+        obs_dict['wave'][0] = np.deg2rad(wave[0])
+        obs_dict['wave'][1] = wave[1]
+        
+        return obs_dict
+
+    def reset(self, seed=None, 
+                    scenario_info=None, 
+                    initial_state=None, 
+                    wind_generator_fn: Union[Callable[[int], np.ndarray], None] = None, 
+                    current_generator_fn: Union[Callable[[int], np.ndarray], None] = None, 
+                    wave_generator_fn: Union[Callable[[int], np.ndarray], None] = None,     
+                    **kwargs):
+        '''
+
+        current_generator_fn (Callable[[int], np.ndarray], optional): Function that returns angle and speed representing the global water current during the simulation. Defaults to None.
+        wind_generator_fn (Callable[[int], np.ndarray], optional): Function that returns angle and speed representing the global wind during the simulation. Defaults to None.
+        wave_generator_fn (Callable[[int], np.ndarray], optional): Function that returns angle and height representing the global wave during the simulation. Defaults to None.
+
+        '''
+        
         super().reset(seed=seed, **kwargs)
         if seed is not None:
             np.random.seed(seed)
         self.step_idx = 0
 
-        wind = self.wind_generator_fn(self.step_idx)
-        water = self.water_generator_fn(self.step_idx)
-        wave = self.wave_generator_fn(self.step_idx)
-
-        # replace this 
-        # self.obs, info = self.sim.reset(wind, water, self.NB_STEPS_PER_SECONDS)
-        self.obs = zero_values
-        info = env_info
+        # ----------------------
+        #  Set vessel state 
+        # ----------------------
         
+        # Automatically create the equivalent dictionary of the Observation class
+        initial_values_obs = {key: np.zeros(value.__args__[0], dtype=np.float32) for key, value in Observation.__annotations__.items()}
+        self.obs = initial_values_obs
+
+        # map initial state to observations dict
+        if initial_state is not None:
+            self.obs = self._map_vessel_state_2_observations(initial_state)
+
+        # ----------------------
+        #  Set environmemnt state 
+        # ----------------------
+
+        self.wind_generator_fn = wind_generator_fn if wind_generator_fn \
+            else self._direction_generator()
+        self.current_generator_fn = current_generator_fn if current_generator_fn \
+            else self._direction_generator(0.01)
+        self.wave_generator_fn = wave_generator_fn if wave_generator_fn \
+            else self._direction_generator(0.01)
+
+        current = self.current_generator_fn(self.step_idx)
+        wind = self.wind_generator_fn(self.step_idx)
+        wave = self.wave_generator_fn(self.step_idx)
+        self.obs = self._map_env_state_2_observations(self.obs, current, wind, wave)
+
+        self.step_idx = 0
+
+        # ----------------------
+        #  Set animation scenario
+        # ----------------------
+        info = default_scenario_info
+        if scenario_info is not None:
+            info = scenario_info
+        
+        # ----------------------------------------
+
         # setup the renderer, its needed to know the min/max position of the boat
         if self.renderer:
             self.renderer.setup(info['map_bounds'] * self.map_scale, info['ref_path'])
@@ -197,7 +294,7 @@ class ShipEnv(VesselEnv):
         if is_debugging_all():
             print('\nResetting environment:')
             print(f'  -> Wind: {wind}')
-            print(f'  -> Water: {water}')
+            print(f'  -> Current: {current}')
             print(f'  -> frequency: {self.NB_STEPS_PER_SECONDS} Hz')
             print(f'  <- Obs: {self.obs}')
             print(f'  <- Info: {info}')
@@ -210,7 +307,7 @@ class ShipEnv(VesselEnv):
         self.step_idx += 1
 
         # replace this 
-        next_obs = fortran_sim_update(self.obs, action, self.sim) 
+        next_obs = sim_update(self.obs, action, self.sim) 
 
         # could implement this to terminate if the end of the reference path has been reached
         terminated = False
